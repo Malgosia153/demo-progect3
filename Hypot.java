6976bfc0848aea02bcdf85abd47a7ca6e7842d0e
Elena-Rybalko <ieliena.rybalko@mail.ru>
2023-10-13 21:51:40 +0300

    refactor hypot

--- Hypot.java
+++ Hypot.java
@@ -1,3 +1,7 @@
-public static double multiply(int a, int b, int c) {
-        int z = a * b * c;
-        return z;*/
+
+public class Hypot {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+    }
+}
